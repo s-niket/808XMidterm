8b305f21626357e22ba6f34e6a415baf31415811
Niket <s.niket28@gmail.com>
2018-10-09 20:40:33 -0400

    [#11] include/Vehicle: Changed the constructor

--- include/Vehicle.hpp
+++ include/Vehicle.hpp
@@ -1,12 +1,12 @@
 /**
  * @file Vehicle.hpp
  * @author Niket Shah
- * @copyright 2018 Niket Shah Zachary Zimits
+ * @copyright 2018 Niket Shah
  * @brief Header for Vehicle class
  */
 
-#ifndef INCLUDE_VEHICLE_HPP_
-#define INCLUDE_VEHICLE_HPP_
+#ifndef INCLUDE_VEHICLE_HPP
+#define INCLUDE_VEHICLE_HPP
 
 #include <iostream>
 #include "Controller.hpp"
@@ -14,43 +14,41 @@
  * @brief Class Vehicle
  * Class defines vehicle attributes and
  * member functions to update vehicle's orientation
- * and velocity to a desired value
+ * and velocity to a desired value 
  */
 
-class Vehicle {
+public class Vehicle{
  private:
-  double wheelDiameter;/*!<diameter of the wheels  */
-  double dTime = 0.05;/*!< Time step for controller inputs */
-  double steeringAngleConstraint;/*!<The steering angle constraint restricting the motion  */
-  double trackWidth;/*!<The distance between the two back wheels  */
-  double wheelBase;/*!<The length between the front and the back wheels  */
-  double currentVelocity = 1;/*!<The current velocity of the vehicle  */
-  double desiredVelocity;/*!<The desired velocity of the vehicle  */
-  double currentOrientation = 0;/*!<dThe current orientation of the vehicle  */
-  double desiredOrientation;/*!<The desired orientation of the vehicle  */
-  Controller pid;/*!<Instance of the controller class  */
+  double wheelDiameter;
+  double steeringAngleConstraint;
+  double trackWidth;
+  double currentVelocity;
+  double desiredVelocity;
+  double currentOrientation;
+  double desiredOrientation;
 
  public:
   /*
    * @brief Constructor of class Vehicle
    */
-  Vehicle(double wheelDiameter, double steeringAngleConstraint,
-          double trackWidth, double wheelBase);
-
-  double setOrientation(double desiredOrient);
-  double setVelocity(double desiredVelo);
-
-
+  Vehicle(double wheelDiameter,double steeringAngleConstraint,
+          double trackWidth);
 
   /*
-   * @brief Method to update the orientation of the vehicle
+   * @brief Method to udpate the orientation of the vehicle
    */
-  double updateOrientation();
+  double updateOrientation(){
+    double _orientation;
+    return _orientation;
+  }
 
   /*
-   * @brief Method to update velocity of the vehicle
+   *  @brief Method to update velocity of the vehicle
    */
-  double updateVelocity();
+  double updateVelocity(){
+    double _velocity;
+    return _velocity;
+  }
 
   /*
    * @brief Method to calculate a controller output and update the vehicle
@@ -75,5 +73,3 @@ class Vehicle {
 
 
 #endif  // INCLUDE_VEHICLE_HPP_
-
-#endif //INLCUDE_VEHICLE_HPP
