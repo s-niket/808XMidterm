a18325c52287be91561d7625d0bf7f676aaf53e7
Niket <s.niket28@gmail.com>
2018-10-09 20:40:33 -0400

    include/Vehicle: Changed the constructor

--- include/Vehicle.hpp
+++ include/Vehicle.hpp
@@ -1,21 +1,22 @@
 /**
  * @file Vehicle.hpp
  * @author Niket Shah
- * @copyright 2018 Niket Shah
+ * @copyright 2018 Niket Shah Zachary Zimits
  * @brief Header for Vehicle class
  */
 
 #ifndef INCLUDE_VEHICLE_HPP
 #define INCLUDE_VEHICLE_HPP
 
+#include <iostream>
 /**
  * @brief Class Vehicle
  * Class defines vehicle attributes and
  * member functions to update vehicle's orientation
- * and velocity to a desired value 
+ * and velocity to a desired value
  */
 
-public class Vehicle{
+class Vehicle {
  private:
   double wheelDiameter;
   double steeringAngleConstraint;
@@ -24,18 +25,30 @@ public class Vehicle{
   double desiredVelocity;
   double currentOrientation;
   double desiredOrientation;
-  
+
  public:
-  
-  Vehicle(double wheelDiameter,double steerinAngleConstraint,
+  /*
+   * @brief Constructor of class Vehicle
+   */
+  Vehicle(double wheelDiameter,double steeringAngleConstraint,
           double trackWidth);
-  
+
+  /*
+   * @brief Method to udpate the orientation of the vehicle
+   */
   double updateOrientation(){
+    double _orientation;
+    return _orientation;
   }
-  
+
+  /*
+   *  @brief Method to update velocity of the vehicle
+   */
   double updateVelocity(){
+    double _velocity;
+    return _velocity;
   }
-  
+
   ~Vehicle();
 };
 
