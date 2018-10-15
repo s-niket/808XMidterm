01b8789cc32f66647c8586bf24d4bde3f8272151
Niket <s.niket28@gmail.com>
2018-10-15 15:47:38 -0400

    [#21] app/ Removed cpplint and cppcheck errors

--- app/Vehicle.cpp
+++ app/Vehicle.cpp
@@ -12,35 +12,94 @@
 /**
  * @brief                Constructor for Vehicle class
  * @param wheelD         Defines the wheel diameters of type double
- * @param steerAngleConstraint Defines the steering angle constraint of type double
+ * @param steerAngleConstraint Defines the steering angle
+ *                             constraint of type double
  * @param trackW          Defines the track-width of the vehicle of type double
  */
 
-Vehicle::Vehicle(double wheelD,double steerAngleConstraint,
-          double trackW){
+Vehicle::Vehicle(double wheelD, double steerAngleConstraint, double trackW,
+                 double wheelB) {
   wheelDiameter = wheelD;
   steeringAngleConstraint = steerAngleConstraint;
   trackWidth = trackW;
+  wheelBase = wheelB;
 }
 
+/**
+ * @brief                Function to set the orientation of the
+ *                       vehicle to a desired value
+ * @param desiredOrient  Defines the desired orientation of type double
+ * @return desiredOrientation Returns the desired orientation of the vehicle
+ */
 
-double Vehicle::updateOrientation(){
-  double _orientation;
+double Vehicle::setOrientation(double desiredOrient) {
+  desiredOrientation = desiredOrient;
+  return desiredOrientation;
+}
+/**
+ * @brief                Function to set the velocity of the
+ *                       vehicle to a desired value
+ * @param desiredVelo    Defines the desired velocity of type double
+ * @return desiredVelocity  Returns the desired velocity of the vehicle
+ */
+double Vehicle::setVelocity(double desiredVelo) {
+  desiredVelocity = desiredVelo;
+  return desiredVelocity;
+}
+/**
+ * @brief                Function to update the orientation of
+ *                       the vehicle to a desired value
+ * @param turnRadius     Defines the radius to execute the turn of type double
+ * @param steeringAngle  Defines the steering angle to execute
+ *                       the turn of type double
+ * @return currentOrientation  Returns the current orientation of the vehicle
+ *                             after the turn of type double
+ */
+double Vehicle::updateOrientation(double turnRadius, double steeringAngle) {
+  double orientation;
   double distanceTraveled = currentVelocity * dTime;
-  double deltaOrientation = (distanceTraveled*360)/(2*M_PI*orientControl.getTurningRadius());
-  if(orientControl.getSteeringAngle()>0)
-	  currentOrientation += deltaOrientation;
+  double deltaOrientation = (distanceTraveled * 360) / (2 * M_PI * turnRadius);
+  if (steeringAngle > 0)
+    orientation = currentOrientation + deltaOrientation;
   else
-	  currentOrientation -= deltaOrientation;
+	  orientation = currentOrientation - deltaOrientation;
+  if (orientation >= 360)
+    currentOrientation = orientation - 360;
+  else if (orientation < 0)
+    currentOrientation = 360 + orientation;
+  else
+    currentOrientation = orientation;
+  return currentOrientation;
+}
+/**
+ * @brief                Function to update the velocity of
+ *                       the vehicle to a desired value
+ * @param newVelocity    Defines the desired velocity of type double
+ * @return currentVelocity  Returns the current velocity of the
+ *                          vehicle after the updation
+ */
+double Vehicle::updateVelocity(double newVelocity) {
+  currentVelocity = newVelocity;
+  return currentVelocity;
+}
+/**
+ * @brief                Function to get the value of orientation
+ * @return currentOrientation  Returns the current orientation of the vehicle
+ */
+double Vehicle::getOrientation() {
   return currentOrientation;
 }
 
-double Vehicle::updateVelocity(){
-  double leftWheelSpeed = orientControl.getLeftWheelSpeed();
-  double rightWheelSpeed = orientControl.getRightWheelSpeed();
-  currentVelocity = (leftWheelSpeed+rightWheelSpeed)/2;
+/**
+ * @brief                Function to get the value of orientation
+ * @return currentOrientation  Returns the current orientation of the vehicle
+ */
+double Vehicle::getVelocity() {
   return currentVelocity;
 }
 
+/*
+ * @brief Destructor for Vehicle class
+ */
 Vehicle::~Vehicle() {
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,32 +1,43 @@
+/**
+ * @file Controller.hpp
+ * @author Niket Shah Zachary Zimits
+ * @copyright 2018 Niket Shah Zachary Zimits
+ * @brief Implementation of a controller for a tricycle
+ *        where it can enable the vehicle to make a turn
+ *        at a specified orientation and run at a specified
+ *        velocity
+ *
+ */
 #include <iostream>
 #include <math.h>
 #include "Vehicle.hpp"
 #include "Controller.hpp"
 
-int main()
-{
-    double delta=10;
-	double steeringAngleConstraint = 45;
-    double wheelDiameter = 2;
-    double trackWidth = 4;
-    double wheelBase = 4;
-    double desiredOrientation = 270;
-    double desiredVelocity=10;
-    Vehicle tricycle(wheelDiameter,steeringAngleConstraint,trackWidth,wheelBase);
-    tricycle.setVelocity(desiredVelocity);
-    tricycle.setOrientation(desiredOrientation);
-    Controller pid(.1,.1,.1,steeringAngleConstraint,wheelDiameter,trackWidth,wheelBase);
+int main() {
+  double delta = 10;
+  double steeringAngleConstraint = 45;
+  double wheelDiameter = 2;
+  double trackWidth = 4;
+  double wheelBase = 4;
+  double desiredOrientation = 90;
+  double desiredVelocity = 10;
+  Vehicle tricycle(wheelDiameter, steeringAngleConstraint, trackWidth,
+                   wheelBase);
+  tricycle.setVelocity(desiredVelocity);
+  tricycle.setOrientation(desiredOrientation);
+  Controller pid(.1, .1, .1, steeringAngleConstraint, wheelDiameter, trackWidth,
+                 wheelBase);
 
-    while(delta>.1||fabs(tricycle.getVelocity()-desiredVelocity)>.5){
-		pid.compute(tricycle.getOrientation(),desiredOrientation,tricycle.getVelocity(),desiredVelocity);
-		tricycle.updateOrientation(pid.getTurningRadius(),pid.getSteeringAngle());
-		tricycle.updateVelocity(pid.getVehicleSpeed());
-		delta = fabs(tricycle.getOrientation()-desiredOrientation);
+  while (delta > .1 || fabs(tricycle.getVelocity() - desiredVelocity) > .5) {
+    pid.compute(tricycle.getOrientation(), desiredOrientation,
+                tricycle.getVelocity(), desiredVelocity);
+    tricycle.updateOrientation(pid.getTurningRadius(), pid.getSteeringAngle());
+    tricycle.updateVelocity(pid.getVehicleSpeed());
+    std::cout << pid.getSteeringAngle() << "\n";
+    delta = fabs(tricycle.getOrientation() - desiredOrientation);
     }
 
-std::cout<<tricycle.getOrientation()<<" "<<tricycle.getVelocity();
+  std::cout << tricycle.getOrientation() << " " << tricycle.getVelocity();
 
-
-	return 0;
-
+  return 0;
 }
